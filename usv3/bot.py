5fb4e15bbcc434c42b9dbb5fb417b2f2b16d323a
carrot <68383195+AnnikaV9@users.noreply.github.com>
2024-09-22 10:02:32 +0800

    Log join on `onlineSet`

--- usv3/bot.py
+++ usv3/bot.py
@@ -26,11 +26,9 @@ class Bot:
         await self.ws.send(json.dumps({"cmd": cmd, **kwargs}))
 
     async def main(self) -> None:
-        channel, server = self.config["channel"], self.config["server"]
-        async with websockets.connect(server, ping_timeout=None) as ws:
+        async with websockets.connect(self.config["server"], ping_timeout=None) as ws:
             self.ws = ws
-            await self.send(cmd="join", nick=f"{self.config['nick']}#{self.config['password']}", channel=channel)
-            print(f"Joined {channel} on {server}")
+            await self.send(cmd="join", nick=f"{self.config['nick']}#{self.config['password']}", channel=self.config["channel"])
             await asyncio.gather(self.ping_loop(), self.receive_loop())
 
     async def ping_loop(self) -> None:
@@ -44,62 +42,78 @@ class Bot:
             resp = json.loads(resp)
             match resp["cmd"]:
                 case "chat":
-                    trip = resp.get("trip")
-                    if trip == "":
-                        trip = None
-
-                    if resp["nick"] != self.config["nick"]:
-                        for handler in self.modules["message"]:
-                            asyncio.create_task(self.modules["message"][handler].run(self, resp["text"], resp["nick"], trip))
-
-                        for command in self.modules["command"]:
-                            if resp["text"].startswith(f"{self.config['prefix']}{command} ") or resp["text"] == f"{self.config['prefix']}{command}":
-                                asyncio.create_task(self.modules["command"][command].run(self, resp["text"], resp["nick"], trip, resp["level"]))
-
-                            if "alias" in self.cmd_map["command"][command]:
-                                if resp["text"].startswith(f"{self.config['prefix']}{self.cmd_map['command'][command]['alias']} ") or resp["text"] == f"{self.config['prefix']}{self.cmd_map['command'][command]['alias']}":
-                                    asyncio.create_task(self.modules["command"][command].run(self, resp["text"], resp["nick"], trip, resp["level"]))
+                    await self.handle_chat(resp)
 
                 case "info":
                     if resp.get("type") == "whisper":
-                        trip = resp.get("trip")
-                        if trip == "":
-                            trip = None
-
-                        if resp["from"] != self.config["nick"] and not resp["text"].startswith("You whispered to"):
-                            text = resp["text"].removeprefix(f"{resp['from']} whispered: ")
-                            for command in self.modules["whisper"]:
-                                if text.startswith(f"{command} ") or text == f"{command}":
-                                    asyncio.create_task(self.modules["whisper"][command].run(self, text, resp["from"], trip, resp["level"]))
-
-                                if "alias" in self.cmd_map["whisper"][command]:
-                                    if text.startswith(f"{self.cmd_map['whisper'][command]['alias']} ") or text == f"{self.cmd_map['whisper'][command]['alias']}":
-                                        asyncio.create_task(self.modules["whisper"][command].run(self, text, resp["from"], trip, resp["level"]))
+                        await self.handle_whisper(resp)
 
                 case "onlineAdd":
-                    trip = resp.get("trip")
-                    if trip == "":
-                        trip = None
-
-                    nick = resp["nick"]
-                    self.online_users.append(nick)
-                    self.online_hashes[nick] = resp["hash"]
-                    self.online_trips[nick] = trip
-                    for handler in self.modules["join"]:
-                        asyncio.create_task(self.modules["join"][handler].run(self, resp["nick"], resp["hash"], trip))
+                    await self.handle_join(resp)
 
                 case "onlineRemove":
-                    nick = resp["nick"]
-                    for handler in self.modules["leave"]:
-                        asyncio.create_task(self.modules["leave"][handler].run(self, resp["nick"]))
-
-                    self.online_users.remove(nick)
-                    self.online_hashes.pop(nick)
-                    self.online_trips.pop(nick)
+                    await self.handle_leave(resp)
 
                 case "onlineSet":
-                    for user in resp["users"]:
-                        nick = user["nick"]
-                        self.online_users.append(nick)
-                        self.online_hashes[nick] = user["hash"]
-                        self.online_trips[nick] = user["trip"] if user["trip"] != "" else None
+                    await self.handle_set(resp)
+
+    async def handle_chat(self, resp) -> None:
+        trip = resp.get("trip")
+        if trip == "":
+            trip = None
+
+        if resp["nick"] != self.config["nick"]:
+            for handler in self.modules["message"]:
+                asyncio.create_task(self.modules["message"][handler].run(self, resp["text"], resp["nick"], trip))
+
+            for command in self.modules["command"]:
+                if resp["text"].startswith(f"{self.config['prefix']}{command} ") or resp["text"] == f"{self.config['prefix']}{command}":
+                    asyncio.create_task(self.modules["command"][command].run(self, resp["text"], resp["nick"], trip, resp["level"]))
+
+                if "alias" in self.cmd_map["command"][command]:
+                    if resp["text"].startswith(f"{self.config['prefix']}{self.cmd_map['command'][command]['alias']} ") or resp["text"] == f"{self.config['prefix']}{self.cmd_map['command'][command]['alias']}":
+                        asyncio.create_task(self.modules["command"][command].run(self, resp["text"], resp["nick"], trip, resp["level"]))
+
+    async def handle_whisper(self, resp) -> None:
+        trip = resp.get("trip")
+        if trip == "":
+            trip = None
+
+        if resp["from"] != self.config["nick"] and not resp["text"].startswith("You whispered to"):
+            text = resp["text"].removeprefix(f"{resp['from']} whispered: ")
+            for command in self.modules["whisper"]:
+                if text.startswith(f"{command} ") or text == f"{command}":
+                    asyncio.create_task(self.modules["whisper"][command].run(self, text, resp["from"], trip, resp["level"]))
+
+                if "alias" in self.cmd_map["whisper"][command]:
+                    if text.startswith(f"{self.cmd_map['whisper'][command]['alias']} ") or text == f"{self.cmd_map['whisper'][command]['alias']}":
+                        asyncio.create_task(self.modules["whisper"][command].run(self, text, resp["from"], trip, resp["level"]))
+
+    async def handle_join(self, resp) -> None:
+        trip = resp.get("trip")
+        if trip == "":
+            trip = None
+
+        nick = resp["nick"]
+        self.online_users.append(nick)
+        self.online_hashes[nick] = resp["hash"]
+        self.online_trips[nick] = trip
+        for handler in self.modules["join"]:
+            asyncio.create_task(self.modules["join"][handler].run(self, resp["nick"], resp["hash"], trip))
+
+    async def handle_leave(self, resp) -> None:
+        nick = resp["nick"]
+        for handler in self.modules["leave"]:
+            asyncio.create_task(self.modules["leave"][handler].run(self, resp["nick"]))
+
+        self.online_users.remove(nick)
+        self.online_hashes.pop(nick)
+        self.online_trips.pop(nick)
+
+    async def handle_set(self, resp) -> None:
+        print(f"Joined {resp['channel']} on {self.config['server']}")
+        for user in resp["users"]:
+            nick = user["nick"]
+            self.online_users.append(nick)
+            self.online_hashes[nick] = user["hash"]
+            self.online_trips[nick] = user["trip"] if user["trip"] != "" else None
