0f89af1b136209b322fac2b61398955688cf8796
carrot <68383195+AnnikaV9@users.noreply.github.com>
2024-10-04 18:21:35 +0800

    Remove `command.coin` module

--- usv3/events/command/scramble.py
+++ usv3/events/command/scramble.py
@@ -13,11 +13,11 @@ class Module:
     def on_load(bot, namespace):
         namespace.solution = None
         with open("assets/words.txt", "r") as wordlist:
-            namespace.wordlist = wordlist.read()
+            namespace.wordlist = wordlist.read().splitlines()
 
     @staticmethod
     async def run(bot, namespace, text, args, sender, trip, ulevel):
-        word = random.choice(namespace.wordlist.splitlines())
+        word = random.choice(namespace.wordlist)
         scrambled_word = "".join(random.sample(word, len(word)))
         namespace.solution = word
         await bot.send(text=f"**Scramble started:** {scrambled_word}")
