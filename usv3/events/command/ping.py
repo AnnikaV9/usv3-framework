573335ac8f0d2f775b7e65815fbe42dffd783b97
carrot <68383195+AnnikaV9@users.noreply.github.com>
2024-10-07 20:37:00 +0800

    Add command.ping module

---
+++ usv3/events/command/ping.py
@@ -0,0 +1,12 @@
+#
+#  Command for measuring server response time.
+#
+
+class Module:
+    description = "Measures server response time"
+
+    @staticmethod
+    async def run(bot, namespace, text, args, sender, trip, ulevel):
+        pong = await bot.ws.ping()
+        latency = await pong
+        await bot.reply(sender, f"{round(latency * 1000)} ms")
